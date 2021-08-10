e46b7c3e49f2c5dfcc10c6d50f51f5a44d50b59c
HumphreysCarter <carter.humphreys@lake-effect.dev>
2021-08-09 19:15:26 -0700

    Updated NCEP ingest

--- grib-api/ingest/ncep.py
+++ grib-api/ingest/ncep.py
@@ -1,36 +1,91 @@
-
 import ftplib
 from datetime import datetime, timedelta
 
-# Get current cycle time
-rtime = datetime(2021, 2, 19, 0)
+class Ingest:
 
-# Servers
-nomads = 'ftp.ncep.noaa.gov'
-ftpprd = 'ftpprd.ncep.noaa.gov'
+    def __init__(self, model, date, downloadPath, server='ftpprd.ncep.noaa.gov', filter=None, include_idx=True, overwrite=False):
+        """
+        Initializes ingest from an AWS S3 bucket for a given bucket and path, to a specified path.
+        """
+        # Set instance variables
+        self.basePath = downloadPath
+        self.vtime = date
+        self.model = model
+        self.server = server
+        self.serverPath = self.get_server_path()
+        self.filter = filter
+        self.idx = include_idx
+        self.overwrite = overwrite
 
-# Model Paths
-hrrr = f'/pub/data/nccf/com/hrrr/prod/hrrr.{rtime:%Y%m%d}/conus/'
-nam  = f'/pub/data/nccf/com/nam/prod/nam.{rtime:%Y%m%d}/'
-gfs  = f'/pub/data/nccf/com/gfs/prod/gfs.{rtime:%Y%m%d}/{rtime:%H}/'
+    def get_server_path(self):
+        """
+        Gets an NCEP FTP path name for a given model
+        """
+        key = {'GFS':f'/pub/data/nccf/com/gfs/prod/gfs.{self.vtime:%Y%m%d}/{self.vtime:%H}/',
+               'NAM':f'/pub/data/nccf/com/nam/prod/nam.{self.vtime:%Y%m%d}/',
+               'HRRR':f'/pub/data/nccf/com/hrrr/prod/hrrr.{self.vtime:%Y%m%d}/conus/'}
 
-# File Filter
-filter = f'hrrr.t{rtime:%H}z.wrfprsf'
+        try:
+            return key[self.model.upper()]
+        except:
+             raise KeyError('Input model is unsupported. Supported models are GFS, NAM, and HRRR.')
 
-# Login to FTP server with anonymous credentials
-ftp = ftplib.FTP(ftpprd)
-ftp.login('anonymous', 'password')
+    def start(self):
+        """
+        Starts ingest from FTP server
+        """
+        # Login to FTP server with anonymous credentials
+        self.ftp = ftplib.FTP(self.server)
+        self.ftp.login('anonymous', 'password')
 
-# Navigate to model directory
-ftp.cwd(hrrr)
+        # Navigate to directory for desired run
+        self.ftp.cwd(self.serverPath)
 
-#
+        # Get list of files
+        self.files = self.ftp.nlst()
 
-for file in ftp.nlst():
-    if filter in file and 'idx' not in file:
-        ftp.retrbinary("RETR " + file, open(f'data/{file}', 'wb').write)
-        print(file)
-        break
+        # Filter based on file filter
+        self.filterList()
 
+        # Download files
+        self.downloadFiles()
 
-ftp.quit()
+        # Close the FTP connection
+        self.ftp.close()
+
+    def filterList(self):
+        """
+        Filters file list based on keywords
+        """
+        # Convert single string filter to single item list
+        if isinstance(self.filter, str):
+            self.filter = [self.filter]
+
+        # Check that each keyword is in each file
+        for keyword in self.filter:
+            self.files = [file for file in self.files if keyword in file]
+
+    def downloadFiles(self):
+        """
+        Downloads files from the FTP server
+        """
+        # Create empty list to store local paths
+        self.file_list = []
+
+        # Build file path
+        path = f'{self.basePath}/{self.model}.{self.vtime:%Y%m%d}/{self.vtime:%H}/'
+        Path(path).mkdir(parents=True, exist_ok=True)
+
+        # Get files
+        for file in self.files:
+
+            # Check if file exists already
+            filepath = f'{path}/{file}'
+            if self.overwrite == True or Path(filepath).exists() == False:
+
+                # Download file over FTP
+                print(f'Downloading {file} from NCEP FTP Server ...')
+                self.ftp.retrbinary("RETR " + file, open(filepath, 'wb').write)
+
+                # Add to file list
+                self.file_list.append(filepath)
